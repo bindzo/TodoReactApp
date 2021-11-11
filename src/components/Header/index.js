b743af448044fcc816d4b04c2d5b4d4ebbe0ee6b
bindzo <hpesc1133@gmail.com>
2021-11-11 09:58:46 +0700

    integrate with graphql

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { AuthContext } from '../../App';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function Header() {
   const {auth, setAuth} = React.useContext(AuthContext)
@@ -27,7 +27,15 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Todo App
           </Typography>
-          {auth && <Button onClick={onLogoutClick} color="inherit">Logout</Button>}
+          {auth &&
+          <div>
+            <Link style={{textDecoration: 'none', color: 'white'}} to="/todogql">
+              <Button color="inherit">GRAPHQL</Button>
+            </Link>
+            <Button onClick={onLogoutClick} color="inherit">Logout</Button>
+          </div>
+          }
+
         </Toolbar>
       </AppBar>
     </Box>
