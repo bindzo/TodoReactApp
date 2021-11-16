d8ab7a48884cf04bda8c5ce58eee2f2a5127073b
bindzo <hpesc1133@gmail.com>
2021-11-17 00:29:06 +0700

    change jest env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   testMatch: ['**/*.test.js'],
