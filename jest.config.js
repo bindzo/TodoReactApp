11492be72b821402d923ec7405d8ed9884679c45
bindzo <hpesc1133@gmail.com>
2021-11-17 09:35:38 +0700

    update jest version, add jest env

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   testMatch: ['**/*.test.js'],
+  testEnvironment: './tests/custom-jest-environment.js'
 };
